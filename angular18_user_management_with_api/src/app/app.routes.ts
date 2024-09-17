e6ef709fa1c7f29b96a7eacbe2a14c46526ce13b
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-09-17 15:45:52 +0530

    login route done

--- angular18_user_management_with_api/src/app/app.routes.ts
+++ angular18_user_management_with_api/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { CreateuserComponent } from './pages/createuser/createuser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }, 
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: ' user-list',
+                component: UserListComponent
+            },
+            {
+                path: 'create-user',
+                component: CreateuserComponent
+            }
+        ]
+    },
+];
